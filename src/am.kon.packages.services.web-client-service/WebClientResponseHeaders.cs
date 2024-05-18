7f56ae37820a4faf3e63392601c4217017b68910
sergey konakov <konakov.sergey@gmail.com>
2024-05-18 14:16:01 +0400

    1. Edit content of README.md with some documentation 2. Adding Headers property to RequestInvocationResult 3. Adding WebClientResponseHeaders class 4. Adding a set of media types

---
+++ src/am.kon.packages.services.web-client-service/WebClientResponseHeaders.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+using System.Net.Http.Headers;
+
+namespace am.kon.packages.services.WebClientService
+{
+    /// <summary>
+    /// Collection of headers and their values returned from the server after a web client request.
+    /// </summary>
+    public class WebClientResponseHeaders : HttpHeaders
+    {
+        
+    }
+}
